4b130140ed0d80521a4c64a9f9c5bc828c093d95
John Kamperakis <johnkamperakis@gmail.com>
2023-12-30 19:52:56 +0200

    Finished the 16th section, added more things into Simplistic Blog

--- Simplistic-Blog/src/app/app-routing.module.ts
+++ Simplistic-Blog/src/app/app-routing.module.ts
@@ -5,11 +5,12 @@ import { SingleCategoryComponent } from './pages/single-category/single-category
 import { ContactUsComponent } from './pages/contact-us/contact-us.component';
 import { TermsAndConditionComponent } from './pages/terms-and-condition/terms-and-condition.component';
 import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { SinglePostComponent } from './pages/single-post/single-post.component';
 
 const routes: Routes = [
   { path:'', component: HomeComponent },
   { path:'category', component: SingleCategoryComponent },
-  { path:'post', component: SingleCategoryComponent },
+  { path:'post', component: SinglePostComponent },
   
   { path:'about', component: AboutUsComponent },
   { path:'term-conditions', component: TermsAndConditionComponent },
