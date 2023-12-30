1b1749d3b4ac7fd60789cf62dd5e5ad74d0036d7
John Kamperakis <johnkamperakis@gmail.com>
2023-12-30 17:56:48 +0200

    Finished the 15th section, done with the Simplistic Blog

--- Simplistic-Blog/src/app/app.module.ts
+++ Simplistic-Blog/src/app/app.module.ts
@@ -15,6 +15,7 @@ import { SubscitionFormComponent } from './subscition-form/subscition-form.compo
 import { CommentFormComponent } from './comments/comment-form/comment-form.component';
 import { CommentListComponent } from './comments/comment-list/comment-list.component';
 import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { PostCardComponent } from './layouts/post-card/post-card.component';
 
 @NgModule({
   declarations: [
@@ -30,7 +31,8 @@ import { AboutUsComponent } from './pages/about-us/about-us.component';
     SubscitionFormComponent,
     CommentFormComponent,
     CommentListComponent,
-    AboutUsComponent
+    AboutUsComponent,
+    PostCardComponent
   ],
   imports: [
     BrowserModule,
