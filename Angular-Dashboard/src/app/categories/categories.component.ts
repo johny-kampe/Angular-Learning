ba450b9ce86b900e6bb658f554735e673575d532
John Kamperakis <giannis.kamperakis@braincube.gr>
2024-01-04 16:46:54 +0200

    Section 18 : Progressed the project Angular-Dashboard, more notes

---
+++ Angular-Dashboard/src/app/categories/categories.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { CategoriesService } from '../services/categories.service';
+import { Category } from '../models/category';
+
+@Component({
+  selector: 'app-categories',
+  templateUrl: './categories.component.html',
+  styleUrls: ['./categories.component.css']
+})
+export class CategoriesComponent implements OnInit {
+  constructor( private categoryService: CategoriesService) { }
+
+  ngOnInit(): void {
+    this.categoryService.loadData().subscribe(val => {
+      console.log(val);
+    })
+  }
+
+  onSubmit(formData: any){
+    let categoryData: Category = {
+      category: formData.value.category
+    }
+
+    this.categoryService.saveData(categoryData)
+
+    // this.afs.collection('categories').add(categoryData).then(docRef => {
+    //   console.log(docRef);
+
+    //   this.afs.doc(`categories/${docRef.id}`).collection('subcategories').add(subCategoryData).then(docRef1 => {
+    //     console.log(docRef1);
+
+    //     this.afs.doc(`categories/${docRef.id}/subcategories/${docRef1.id}`).collection('subsubcategories').add(subCategoryData).then(docRef2 => {
+    //       console.log(docRef2);
+          
+    //     })
+    //   })
+        //   console.log(docRef1);
+      // this.afs.collection('categories').doc(docRef.id).collection('subcategories').add(subCategoryData).then(docRef1 => {
+      //   console.log(docRef1);
+
+      //   this.afs.collection('categories').doc(docRef.id).collection('subcategories').doc(docRef1.id).collection('subsubcategories').add(subCategoryData).then(docRef2 => {
+      //     console.log(docRef2);
+          
+      //   })
+      // })
+      
+    // }).catch(err => {
+    //   console.log(err);
+    // })
+
+  }
+}
---
+++ Angular-Dashboard/src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: true,
+    firebaseConfig: {
+        apiKey: "AIzaSyBtSLP9nffWzEiMT1eVMGtsg8vQM5esybA",
+        authDomain: "angular-dashboard-1bc0e.firebaseapp.com",
+        projectId: "angular-dashboard-1bc0e",
+        storageBucket: "angular-dashboard-1bc0e.appspot.com",
+        messagingSenderId: "843054979359",
+        appId: "1:843054979359:web:27f8a68c9a6af89a77382a",
+        measurementId: "G-RHY2HNEJ13"
+      }
+};
